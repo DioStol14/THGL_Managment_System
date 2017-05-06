fad0b7299e768916897845b33c614452988f7c76
DioStol14 <dionisis.stolis.92@gmail.com>
2017-05-06 16:30:40 +0300

    Form7(Katwxirwsi) ok

--- THGL_Managment_System/Form7.cs
+++ THGL_Managment_System/Form7.cs
@@ -10,11 +10,37 @@ using System.Windows.Forms;
 
 namespace THGL_Managment_System
 {
+    
     public partial class Form7 : Form
     {
+        int posotita;
         public Form7()
         {
             InitializeComponent();
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            posotita++;
+            if (posotita < 0)
+            {
+                posotita = 0;
+                bunifuMetroTextbox1.Text = Convert.ToString(posotita);
+            }
+            else
+                bunifuMetroTextbox1.Text = Convert.ToString(posotita);
+        }
+
+        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        {
+            posotita--;
+            if (posotita < 0)
+            {
+                posotita = 0;
+                bunifuMetroTextbox1.Text = Convert.ToString(posotita);
+            }
+            else
+                bunifuMetroTextbox1.Text = Convert.ToString(posotita);
         }
     }
 }
